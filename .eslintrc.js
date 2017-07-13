46412f31e8eef80e8338e0d97294dbc784c3d932
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-07-13 12:11:10 -0600

    project start + base64

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+      "ecmaVersion": 2016
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2,
+            {
+              "SwitchCase": 1,
+              "VariableDeclarator": {
+                "var": 2,
+                "let": 2,
+                "const": 3
+              }
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
