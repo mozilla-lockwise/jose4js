d0480a722ec8b42fa4ca77a85e5e0286031a15f7
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-07-15 08:55:50 +0200

    webcrypto ponyfill

---
+++ test/util/webcrypto-test.js
@@ -0,0 +1,15 @@
+/*!
+ *
+ */
+
+var assert = require("chai").assert;
+
+const webcrypto = require("../../lib/util/webcrypto");
+
+describe("util/webcrypto", () => {
+  it("tests existence", () => {
+    assert.exists(webcrypto);
+    assert.typeOf(webcrypto.getRandomValues, "function");
+    assert.typeOf(webcrypto.subtle, "object");
+  });
+});
